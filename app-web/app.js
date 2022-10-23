b3eef39aaf11bd396a9e00beb863fd4d2c04d80a
Benjamngarcia <bperezg1800@alumno.ipn.mx>
2022-10-23 07:53:38 -0500

    use hbs

--- app-web/app.js
+++ app-web/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const cheerio = require('cheerio');
 const request = require('request');
 const bodyParser = require('body-parser');
@@ -11,40 +12,44 @@ app.set('port', process.env.PORT || 5000)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 const robots = robotsParser();
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+app.get('/', (req, res) => res.render('index', { layout: 'main' }));
 app.get('/search', async (req, res) => {
 
-  const { tag } = req.query;
-  let datas = [];
+    const { tag } = req.query;
+    let datas = [];
 
-  //check robots.txt
-  await robots.useRobotsFor(tag)
-  if (await robots.canCrawl(tag)) {
-      request(tag, (err, response, html) => {
-          if (response.statusCode === 200) {
-              const $ = cheerio.load(html);
-              $('script').remove();
-              $('noscript').remove();
-              $('style').remove();
-              $('body').each((i, el) => {
-                  //get all text into body tag
-                  const content = $(el).find("*").text()
+    //check robots.txt
+    await robots.useRobotsFor(tag)
+    if (await robots.canCrawl(tag)) {
+        request(tag, (err, response, html) => {
+            if (response.statusCode === 200) {
+                const $ = cheerio.load(html);
+                $('script').remove();
+                $('noscript').remove();
+                $('style').remove();
+                $('body').each((i, el) => {
+                    //get all text into body tag
+                    const content = $(el).find("*").text()
 
-                  let data = {
-                      content
-                  }
-                  datas.push(data);
-              })
-          }
-          // console.log(Object.getOwnPropertyNames(datas));
-          // console.log(typeof(datas[0].content));
-          console.log(datas);
-      })
-  } else {
-      console.log('Sorry, web scraping is not allowed on this route.')
-  }
+                    let data = {
+                        content
+                    }
+                    datas.push(data);
+                })
+            }
+            // console.log(Object.getOwnPropertyNames(datas));
+            // console.log(typeof(datas[0].content));
+            console.log(datas);
+            res.render('list', { datas })
+        })
+    } else {
+        console.log('Sorry, web scraping is not allowed on this route.')
+    }
 })
 
 // setTimeout(async () => {
@@ -95,7 +100,7 @@ app.get('/search', async (req, res) => {
 //     }
 //     datas.push(data);
 //   })
-  
+
 //   /* let doc = new DOMParser().parseFromString(html, 'text/html'),
 //     text = doc.body.textContent || '';
 //   // Limpiamos los espacios
@@ -139,6 +144,6 @@ app.get('/search', async (req, res) => {
 // }, 150);
 
 app.listen(app.get('port'), () => {
-  console.log(`Server running on port ${app.get('port')}`);
+    console.log(`Server running on port ${app.get('port')}`);
 })
 
